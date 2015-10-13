a21e2d938c53603ca09ab09d40cfbb15582f9467
Lucien Aubert <lu.aubert84@gmail.com>
2015-10-13 19:45:28 +0200

    Oubli dans le merge.

--- Ship.h
+++ Ship.h
@@ -8,14 +8,9 @@ public:
 	Ship( Engine E );
 	~Ship();
 	void draw();
-<<<<<<< HEAD
 	void update();
 	void head_to(int x, int y);
 	void gravitate();
-=======
-	void fillRect(SDL_Rect* rc, int r, int g, int b);
-	void move_to(int target_x, int target_y);
->>>>>>> fc98453b2e7a21d0323b773cd4bf0e6ab14b9dfb
 	int get_faction();
 	bool is_traveling();
 	Engine E;
